29070a8be67336861f25b7c31c78a5a9a0ff5953
Nitesh <05niteshraj@gmail.com>
2023-03-17 17:40:19 +0530

    database queries for controllers

---
+++ src/query/twitdb.query.js
@@ -0,0 +1,22 @@
+const QUERY = {
+
+    USER: {
+        SELECT_ALL: "SELECT * FROM Users",
+        SELECT: "SELECT * FROM Users WHERE user_id = ?",
+        CREATE: "INSERT INTO Users(name, email) VALUES(?,?)",
+        UPDATE: "UPDATE Users SET name = ?, email = ?",
+        DELETE: "DELETE FROM Users WHERE user_id = ?"
+    },
+    TWIT: {
+        SELECT_ALL: "SELECT * FROM Twits",
+        SELECT: "SELECT * FROM Twits WHERE twit_id = ?",
+        CREATE: "INSERT INTO Twits(user_id, title, description) VALUES(?,?,?)",
+        UPDATE: "UPDATE Twits SET name = ?, email = ?",
+        DELETE: "DELETE FROM Twits WHERE twit_id = ?"
+    },
+    COMMENT: {
+
+    }
+};
+
+export default QUERY;
