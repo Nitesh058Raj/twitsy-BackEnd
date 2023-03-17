4b8f8b0c0de25ac1fe7aeb3458519fb4f912f214
Nitesh <05niteshraj@gmail.com>
2023-03-17 17:03:41 +0530

    importing Response object and implementing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import Response from "./domain/response";
+
 dotenv.config();
 
 const PORT = process.env.SERVER_PORT || 3000;
@@ -15,7 +17,7 @@ app.use(cors({origin: '*'}));
 app.use(express.json());
 
 app.get('/', (req,res) => {
-    res.send({message : 'UP'});
+    res.send(new Response(200, 'OK', 'Message: v1.0.0'));
 });
 // process.env --> ansible facts (works link)
 app.listen(PORT, () => console.log(`Server is running on : ${ip.address()} : ${PORT}`));
