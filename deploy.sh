06069091332eaccab8731a6d123a0bb1051742fa
Nitesh <05niteshraj@gmail.com>
2023-03-18 18:19:16 +0530

    commit by script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /home/NextJS/database
 sudo git pull
 sudo docker-compose down -v --remove-orphans
-sudo docker rmi nodeapp_jwt:v1
+sudo docker rmi node_twitsy_backend:v1
 sudo docker-compose up -d
