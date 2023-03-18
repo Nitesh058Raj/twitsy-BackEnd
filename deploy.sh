38d5dadce576216857794f7e5e717f174e38242a
Nitesh <05niteshraj@gmail.com>
2023-03-18 18:07:10 +0530

    commit by script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-rm -rf ../database
-git pull
-docker-compose down -v --remove-orphans
-docker rmi nodeapp_jwt:v1
-docker-compose up -d
+sudo rm -rf /home/NextJS/database
+sudo git pull
+sudo docker-compose down -v --remove-orphans
+sudo docker rmi nodeapp_jwt:v1
+sudo docker-compose up -d
